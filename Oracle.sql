621d869edf3cc1f92f60ccbad51a821f6cdce527
truonghuy1011 <huyhieu10.11.1999@gmail.com>
2021-08-20 11:46:58 +0700

    Oracle

---
+++ Oracle.sql
@@ -0,0 +1,69 @@
+use oracle
+go
+--bang s_region
+create table s_region
+{
+region_id
+}
+go
+
+create table s_region
+{
+
+}
+go
+
+create table s_region
+{
+
+}
+go
+
+create table s_region
+{
+
+}
+go
+
+create table s_region
+{
+
+}
+go
+
+create table s_region
+{
+
+}
+go
+
+create table s_region
+{
+
+}
+go
+
+create table s_region
+{
+
+}
+go
+
+create table s_region
+{
+
+}
+go
+
+create table s_region
+{
+
+}
+go
+
+create table s_region
+{
+
+}
+go
+
